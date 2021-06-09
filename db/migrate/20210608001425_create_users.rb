882bad2f03788863ec5466313d0e46b19cba655d
Marco <marcoparra311@gmail.com>
2021-06-08 20:34:43 -0400

    Setup database and create sidebar

--- db/migrate/20210608001425_create_users.rb
+++ db/migrate/20210608001425_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :username
       t.string :fullname
       t.string :photo
-      t.sting :coverImage
+      t.string :coverImage
 
       t.timestamps
     end
