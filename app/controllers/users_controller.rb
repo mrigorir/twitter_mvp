fc5d307f395ba515bd931ad4b864052942eedfff
Marco <marcoparra311@gmail.com>
2021-06-11 18:04:09 -0400

    Add avatar upload feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_login, only: %i[show edit update]
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_login, only: %i[show follow_user edit update]
+  before_action :set_user, only: [:show]
   
   def new
     @user = User.new
@@ -17,13 +17,17 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit; end
+  def edit
+    @user = User.find(current_user.id)
+  end
 
   def show 
     @user_opinions = set_user.created_opinions.ordered_by_most_recent.all
+    #@user = User.includes(:user_followers).includes(:user_followings).includes(:created_opinions).find(params[:id])
   end
 
   def update
+    @user = User.find(current_user.id)
     if @user.update(user_params)
       flash[:notice] = 'Profile updated!'
       redirect_to edit_user_path(current_user.id)
@@ -50,6 +54,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require('user').permit(:username, :fullname)
+    params.require('user').permit(:username, :fullname, :avatar)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { maximum: 10 }
   validates :fullname, presence: true, length: { maximum: 60 }
   before_save { username.downcase! }
+  has_one_attached :avatar
 
   has_many :created_opinions, foreign_key: :author_id, class_name: 'Opinion'
 
@@ -11,10 +12,9 @@ class User < ApplicationRecord
   has_many :followings, foreign_key: :following_id, class_name: 'Follow'
   has_many :user_followings, through: :followings, source: :following
 
+
   def not_following
-    User.where.not(id: user_followings.select(:id)).where.not(id: id).order(created_at: :desc)
-    #ids = user_followings.find_by(:id)
-    #User.where("id NOT IN ?", ids)
+    User.all.where.not(id: user_followings.select(:id)).where.not(id: id).order(created_at: :desc)
   end
 
   def follow_user(user_id)
